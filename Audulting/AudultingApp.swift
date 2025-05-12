bb86fcc61768cffaa80ec6630533b875d5332685
Maya Stein <mayarosestein@gmail.com>
2025-05-11 20:59:24 -0400

    Initial Commit

---
+++ Audulting/AudultingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudultingApp.swift
+//  Audulting
+//
+//  Created by Maya Stein on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AudultingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudultingTests/AudultingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudultingTests.swift
+//  AudultingTests
+//
+//  Created by Maya Stein on 5/11/25.
+//
+
+import Testing
+@testable import Audulting
+
+struct AudultingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
